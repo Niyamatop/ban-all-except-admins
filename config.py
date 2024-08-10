d4fcaccafea6ce28e4fe3272ffd2ce8e0ba28c78
Dark souls <87437573+Niyamatop@users.noreply.github.com>
2024-08-10 15:30:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("28298365"))
+    API_HASH = os.environ.get("9647ab7fc03a8a93f701a5528b2f206e")
+    BOT_TOKEN = os.environ.get("7391048499:AAG65zd1gUvTOePog_4nRYAd_YWsZR9S7yo")
